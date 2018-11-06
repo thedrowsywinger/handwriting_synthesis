d7be9d30e346019be9bcf143d42aa0d2f023a31c
Habibur Rahman Dipto <jhondipto@gmail.com>
2018-11-06 23:49:57 +0600

    Update temp.py
    1st

---
+++ temp.py
@@ -0,0 +1,35 @@
+"""
+
+./demo.py
+"""
+
+__name__ == '__main__'
+    #Call Hand method
+    Hand():
+    
+    #Create a neural network object with this parameter
+     nn = rnn(
+            log_dir='logs',
+            checkpoint_dir='checkpoints',
+            prediction_dir='predictions',
+            learning_rates=[.0001, .00005, .00002],
+            batch_sizes=[32, 64, 64],
+            patiences=[1500, 1000, 500],
+            beta1_decays=[.9, .9, .9],
+            validation_batch_size=32,
+            optimizer='rms',
+            num_training_steps=100000,
+            warm_start_init_step=17900,
+            regularization_constant=0.0,
+            keep_prob=1.0,
+            enable_parameter_averaging=False,
+            min_steps_to_checkpoint=2000,
+            log_interval=20,
+            logging_level=logging.CRITICAL,
+            grad_clip=10,
+            lstm_size=400,
+            output_mixture_components=20,
+            attention_mixture_components=10
+        )
+        # And then restore it..
+        nn.restore()
